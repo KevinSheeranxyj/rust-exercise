1965c926aacde8c893cce94f3f72f91cbcf0f279
Kevin Xie <kevin.xie@dtcpay.com>
2024-03-29 18:58:13 +0800

    UPDATE: init project

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+
+extern crate log;
+
+fn main() {
+    env_logger::init();
+    error!("Error message");
+   // warn!("warning message");
+   // info!("information message");
+   // debug!("debugging message");
+   // println!("Hello, world!");
+}
